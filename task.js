3a16efd9cb779848db118ebb8ccd234af2d136f4
Maksym <tsnula50@gmail.com>
2026-01-29 17:53:24 +0300

    feat(js): Условия и логические проверки задач

---
+++ task.js
@@ -0,0 +1,16 @@
+let title = ""
+
+if(title === ""){
+    console.log("Название задачи пустое")
+}else{
+    console.log("Задача:", title)
+}
+
+let tasks = 5
+if(taskas === 0){
+    console.log("Список пуст!")
+}else if(tasks <= 3){
+    console.log("Немного задач")
+}else{
+    console.log("Много задач")
+}
